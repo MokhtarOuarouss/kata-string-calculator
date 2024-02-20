4793ec7211ea2c46cd7edc7fde22b3fcbb97000a
Mk Ouarouss <mokhtar.ouarouss.10@gmail.com>
2024-02-20 10:54:42 +0100

    Question 2

--- src/test/java/fr/norsys/stringcalculator/ClazzTest.java
+++ src/test/java/fr/norsys/stringcalculator/ClazzTest.java
@@ -20,4 +20,17 @@ public class ClazzTest {
 
     }
 
+    @Test
+    @DisplayName("Giving an unknow amount of numbers string ")
+    public void Giving_unknow_of_numbers() {
+
+        Clazz clazz = new Clazz();
+
+        Assertions.assertEquals(5,clazz.Add("1,5,6,8,10"));
+        Assertions.assertEquals(8,clazz.Add("11,12,555,96,0,6,7,80"));
+        Assertions.assertEquals(16,clazz.Add("1,5,6,8,10,1,2,4,7,8,5,9,6,3,2,5"));
+        
+
+    }
+
 }
